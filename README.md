d3316a8c27ee9e1e4605e09d591e1c6588a35143
mandal646 <priyanshukumar02023@gmail.com>
2026-03-20 11:31:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# task13
+// Javascript - check if a number is even or odd.
+
+//it contains the simple index.html and script.js file that display the number is even or odd on the console screen.
---
+++ script.js
@@ -0,0 +1,27 @@
+let a = 5;
+if(a%2==0){
+    console.log("the number", a, "you entered is even");
+}else{
+    console.log("the number",a,"you entered is odd");
+}
+
+let b = 10;
+if(b%2==0){
+    console.log("the number",b,"you entered is even");
+}else{
+    console.log("the number",b,"you enterd is odd");
+}
+
+const num = 12;
+if(num%2==0){
+    console.log("the number",num,"you entered is even");
+}else{
+    console.log("the number",num,"you enterd is odd");
+}
+
+const Number = 11;
+if(Number%2==0){
+    console.log("the number",Number,"you entered is even");
+}else{
+    console.log("the number",Number,"you enterd is odd");
+}
